00f1d1791459e510e9bfb1124d8ceba85786dbe1
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-16 05:24:11 +0300

    Declare command interface in Package module.

--- src/src.d.ts
+++ src/src.d.ts
@@ -4,4 +4,14 @@ import Library from "./Library";
 declare module Package {
     type CLI = typeof CLI;
     type Library = typeof Library;
+
+    interface Command {
+        run: (cli: CLI, lib: Library) => void,
+        details: {
+            name: string,
+            description: string,
+            path?: string,
+        },
+        args: any,
+    }
 }
