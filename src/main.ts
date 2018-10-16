d6e0b798c11426362c50189873e2f79547b36f35
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-16 07:37:13 +0300

    List available commands in main.ts.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ const command = process.argv[2];
 
 if (!Library.isCommand(command)) {
     CLI.error(`Unknown command! (${command})`);
-    CLI.info('Use --help to view a list of commands');
+    CLI.info('Available commands:');
+    Library.commands.forEach((name) => {
+        CLI.info(name);
+    });
     process.exit(1);
 }
 
