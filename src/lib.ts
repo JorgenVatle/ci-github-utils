3e955f039ab6edf42eeeb0a0706de36e029591ce
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-16 04:41:37 +0300

    Rework library exports.

--- src/lib.ts
+++ src/lib.ts
@@ -1,16 +1,33 @@
-import * as CLI from "cli";
-
-const cli = CLI.parse({
-    repo: ['r', 'GitHub repository'],
-    hash: ['h', 'GitHub commit hash/sha'],
-    owner: ['o', 'GitHub repository owner username']
-}, [
-    'commit-message'
-]);
+import Commands from "./commands";
 
 export default {
-    cli,
-    output(message: string) {
-        console.log(message);
+
+    /**
+     * Find command by name.
+     *
+     * @param name
+     */
+    findCommand(name: string) {
+        return Commands.find((command) => {
+            return command.details.name === name;
+        });
     },
+
+    /**
+     * Checks if the given command exists.
+     *
+     * @param name
+     */
+    isCommand(name: string) {
+        return !!this.findCommand(name);
+    },
+
+    /**
+     * Prepare new exception.
+     *
+     * @param message
+     */
+    exception(message: string) {
+        return new Error(message);
+    }
 }
